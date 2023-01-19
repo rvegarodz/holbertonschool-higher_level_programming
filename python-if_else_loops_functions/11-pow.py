00d3e4b5182825a3e94831217193486b6347bf5f
rvegarodz <profe.r.vega21@gmail.com>
2023-01-19 15:15:26 -0400

    adding file for task 11

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if (a <= 0 or a >= 0) and (b >= 0):
-        for i in range(b):
-            result = a * a
+    base = a
+    if b == 0:
+        return 1
+    elif b > 0:
+        for i in range(1, b):
+            base *= a
+        return base
+    elif b < 0:
+        b *= -1
+        for i in range(1, b):
+            base *= a
+        result = 1/base
         return result
-    elif (a <= 0 or a >= 0) and (b < 0):
-        for i in range(b):
-            result = a * a
-            positive_result = 1/result
-        return positive_result
+
+print(pow(100, -2))
