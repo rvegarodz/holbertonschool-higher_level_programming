73e7dd5af65ddd786eb1fa39dbb43291b17bbbe0
rvegarodz <profe.r.vega21@gmail.com>
2023-01-19 14:08:36 -0400

    adding file for task 9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    num_str = repr(number)
-    last_digit = num_str[-1]
-
-    print(last_digit, end="")
-    return last_digit
+    if number <= 0 or number >= 0:
+        num_str = repr(number)
+        last_digit = num_str[-1]
+        print(last_digit, end="")
+        return last_digit
