ceedfc81d1b6742188c8ccc0c06ff8dade355cf6
rvegarodz <profe.r.vega21@gmail.com>
2023-01-20 15:58:24 -0400

    adding file for task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    lenght = len(sys.argv)
+
+    if lenght == 1:
+        print("0 arguments.")
+    elif lenght == 2:
+        print ("1 argument:")
+        print ("1: {}".format(sys.argv[lenght - 1]))
+    else:
+        print ("{} arguments:".format(lenght - 1))
+        
+        for i in range(1, lenght):
+            print("{}: {}".format(i, sys.argv[i]))
