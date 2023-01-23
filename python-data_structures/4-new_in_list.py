9a8a3c3c71e5272cdd6570103d7a7820c182c518
rvegarodz <profe.r.vega21@gmail.com>
2023-01-23 15:56:32 -0400

    adding file 4 edited

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -7,7 +7,7 @@ def new_in_list(my_list, idx, element):
         for values in my_list:
             if idx < 0:
                 return my_list
-            elif idx > 0:
+            elif idx => 0:
                 copy_list = my_list.copy()
                 if idx < len_list:
                     copy_list[idx] = element
