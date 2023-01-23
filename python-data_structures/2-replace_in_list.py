25f8fcc822f82627eb6494d2cca569612bdaf873
rvegarodz <profe.r.vega21@gmail.com>
2023-01-23 15:18:05 -0400

    edited again for PEP8

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     len_list = len(my_list)
-    
+
     if idx >= 0 and idx < len_list:
-       my_list[idx] = element
-       return my_list
+        my_list[idx] = element
+        return my_list
     else:
         return my_list
