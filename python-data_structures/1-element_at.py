0dedd867a54f25597f5aa4eedbcda4b96ca75203
rvegarodz <profe.r.vega21@gmail.com>
2023-01-23 14:59:30 -0400

    adding file for task 1

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    len_list = len(my_list)
+    
+    for values in my_list:
+        if idx >= 1 and idx <= len_list:
+            return my_list[idx]
+        else:
+            return None
