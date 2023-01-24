ff750e4821bbb4604d7b936fe7b3b7d6bb0d7285
rvegarodz <profe.r.vega21@gmail.com>
2023-01-24 13:18:43 -0400

    adding file for task 8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence is None:
+        new_tuple = (len(sentence), None)
+    else:
+        new_tuple = (len(sentence), sentence[0])
+    return new_tuple
