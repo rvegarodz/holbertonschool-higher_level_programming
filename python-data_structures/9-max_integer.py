c11f00c2c6550f6dbe1c606878557b2749530bc8
rvegarodz <profe.r.vega21@gmail.com>
2023-01-24 13:30:54 -0400

    adding file for task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    else:
+        max_num = my_list[0]
+        for value in my_list:
+            if value > max_num:
+                max_num = value
+        return max_num
