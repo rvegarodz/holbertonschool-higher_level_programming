218b360f7b8de018c9167a622939e66b6bd204e8
rvegarodz <profe.r.vega21@gmail.com>
2023-01-24 13:21:21 -0400

    file 8 edited

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
+    if sentence is None or (len(sentence) == 0):
         new_tuple = (len(sentence), None)
     else:
         new_tuple = (len(sentence), sentence[0])
