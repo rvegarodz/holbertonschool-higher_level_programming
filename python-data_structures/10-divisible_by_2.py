d0cccd7a865d290894282cfb5f6fde67b959cc44
rvegarodz <profe.r.vega21@gmail.com>
2023-01-24 14:59:14 -0400

    adding file for task 10 edited

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,6 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     if my_list:
-        len_list = len(my_list)
         for values in my_list:
             if (values % 2) == 0:
                 new_list.append(True)
