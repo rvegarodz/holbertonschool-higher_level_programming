998a6cc1d0a6699bb210582353f736607e9257b5
rvegarodz <profe.r.vega21@gmail.com>
2023-01-25 15:07:18 -0400

    adding file for task 4

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+def only_diff_elements(set_1, set_2):
+    new_set = set_1.union(set_2)
+    for value in set_1:
+        for value2 in set_2:
+            if value == value2:
+                new_set.remove(value)
+    return new_set
