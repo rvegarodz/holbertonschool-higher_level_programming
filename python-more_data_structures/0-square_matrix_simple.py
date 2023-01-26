0c7d925b301f2d89885fd983f8f047870de25370
rvegarodz <profe.r.vega21@gmail.com>
2023-01-26 17:28:23 -0400

    adding files 0 and 1 edited for PEP8

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -5,6 +5,5 @@ def square_matrix_simple(matrix=[]):
         for value in lyst:
             lyst_copy.append(value * value)
         return lyst_copy
-    
     new_matrix = list(map(pow, matrix))
     return new_matrix
