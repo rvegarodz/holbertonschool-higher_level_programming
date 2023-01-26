f812e088a5b812efab48a62123bf9e85140d4bf6
rvegarodz <profe.r.vega21@gmail.com>
2023-01-26 17:08:08 -0400

    adding file for task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dictionary = {"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000,}
+    str_len = len(roman_string)
+    if str_len == 1:
+        return roman_dictionary.get(roman_string)
+    else:
+        namor = reversed(roman_string)
+        namor_list = []
+        for letter in namor:
+            namor_list.append(roman_dictionary.get(letter))
+        old_num = 0
+        total_sum = 0
+        for i in namor_list:
+            if i < old_num:
+                total_sum -= i
+            else:
+                total_sum += i
+            old_num = i
+        
+        return total_sum
