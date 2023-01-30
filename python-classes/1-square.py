645812a09a3e4284119a58b05500f9ac319345b9
rvegarodz <profe.r.vega21@gmail.com>
2023-01-30 11:16:38 -0400

    adding file for task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Di"""
+
+
+class Square:
+    """Defines a class as Square
+    Args:
+        size: 
+        """
+    def __init__(self, size):
+        self.__Square__size = size
+    
