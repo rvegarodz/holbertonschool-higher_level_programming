7023505980f59025d271f3a7e539d82f5bec56ac
rvegarodz <profe.r.vega21@gmail.com>
2023-01-31 13:27:18 -0400

    file 3 edited for PEP8

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,12 +3,18 @@
 
 
 class Square:
-    """Defines a class as Square with private argument named size"""
+    """Defines a class as Square with privates arguments (size and position).
+
+    Args:
+        param1: size
+
+    """
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         return (self.__size * self.__size)
