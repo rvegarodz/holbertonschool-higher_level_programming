42f97af88ac569c9d6a8461c69ce5d0eac3ee6e4
rvegarodz <profe.r.vega21@gmail.com>
2023-02-01 14:35:15 -0400

    adding files 0 PEP8 edited

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-""" Comment """
+"""This module provide a function called add_integer()"""
 
 
 def add_integer(a, b=98):
-    """ Comment """
+    """This function sum 2 values
+    Args:
+        a: integer or float value
+        b: integer or float value
+    Return:
+        The return value is the sum of a and b
+    Raises:
+        TypeError: If a or b are not integer or float
+
+    """
     if type(a) != int and type(a) != float:
-         raise TypeError('a must be an integer')
+        raise TypeError('a must be an integer')
     if type(b) != int and type(b) != float:
         raise TypeError('b must be an integer')
     return int(a) + int(b)
