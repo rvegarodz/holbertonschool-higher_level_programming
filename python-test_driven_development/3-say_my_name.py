fd74d6fadb183822687f997d33b77e053689a091
rvegarodz <profe.r.vega21@gmail.com>
2023-02-02 13:53:55 -0400

    adding file for task 2

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""COMMENT"""
+
+
+def say_my_name(first_name, last_name=""):
+    """COMMENT"""
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
