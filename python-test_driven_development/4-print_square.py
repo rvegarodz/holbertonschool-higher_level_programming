bd0c8e93c33c392c66b7344cf5cf09a7e8b7780d
rvegarodz <profe.r.vega21@gmail.com>
2023-02-02 14:58:03 -0400

    adding file for task 3

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""COMMENT"""
+
+
+def print_square(size):
+    """COMMENT"""
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
