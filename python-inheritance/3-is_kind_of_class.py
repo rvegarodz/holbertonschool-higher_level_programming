a0a615c0f8c5bda331403f41892b5f523044e380
rvegarodz <profe.r.vega21@gmail.com>
2023-02-08 16:25:28 -0400

    adding file for task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""COMMENT"""
+
+
+def is_kind_of_class(obj, a_class):
+    """COMMENT"""
+    if isinstance(obj, a_class) is True:
+        return True
+    else:
+        return False
