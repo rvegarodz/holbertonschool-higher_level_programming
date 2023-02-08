fd09c1efd6f9942ece8de087a0aa8e272ce717ce
rvegarodz <profe.r.vega21@gmail.com>
2023-02-08 10:34:42 -0400

    file for task 0 edited

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,6 +2,7 @@
 """This module provide a class named Rectangle"""
 
 def lookup(obj):
+    """Function that returns the list of available attributes and methods of obj"""
     new_list = []
     new_list.append(dir(obj))
     return new_list
