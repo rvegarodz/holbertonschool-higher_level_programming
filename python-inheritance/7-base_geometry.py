27d71789a29b10b6cf772bac4f7325b2541f5059
rvegarodz <profe.r.vega21@gmail.com>
2023-02-09 13:19:39 -0400

    adding files for task 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" COMMENT """
+
+
+class BaseGeometry:
+    """ COMMENT """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        else:
+            if value <= 0:
+                raise ValueError("{} must be greater than 0".format(name))
