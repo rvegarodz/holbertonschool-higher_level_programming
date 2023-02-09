c09f707dd6548f6c89f54f96d4488a77f353617b
rvegarodz <profe.r.vega21@gmail.com>
2023-02-09 13:55:49 -0400

    adding file for task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""COMMENT"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class Rectangle(BaseGeometry):
+    """COMMENT"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__height = height
+        self.__width = width
