4a99ac580ef1b1bf36065a2fb1bcd45cb1876438
rvegarodz <profe.r.vega21@gmail.com>
2023-02-10 09:18:04 -0400

    editing file

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
     def __init__(self, size):
         super().integer_validator("size", size)
         super().__init__(size, size)
-        self._Square__size = size
+        self.__size = size
 
     def __str__(self):
-        return f'[Square] {self._Square__size}/{self._Square__size}'
+        return f'[Square] {self.__size}/{self.__size}'
