00f0aeadd1b4d343ca86a8ce0afd91ab0d809230
rvegarodz <profe.r.vega21@gmail.com>
2023-02-11 14:41:51 -0400

    file for task 1 edited for pep8

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    
+    """ Function that writes a str to a txt file, return lenght of str"""
     with open(filename, "w+", encoding="utf-8") as file:
         return file.write(text)
