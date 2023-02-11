d2b3d752d31144e46d31c7963f517460b1c5abc4
rvegarodz <profe.r.vega21@gmail.com>
2023-02-11 14:54:00 -0400

    adding with statement to file 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 def append_write(filename="", text=""):
     """Function that appends a str at the end of a txt file,
     return lenght of str"""
-    open(filename, "a", encoding="utf-8") as file:
+    with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
