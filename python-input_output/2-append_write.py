712dc7662af39760cc589253d0e921c26917b393
rvegarodz <profe.r.vega21@gmail.com>
2023-02-11 14:53:17 -0400

    file for task 2 edited for pep8

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" Module provide a function called append_write()"""
+"""Module provide a function called append_write()"""
 
 
 def append_write(filename="", text=""):
-    with open(filename, "a", encoding="utf-8") as file:
+    """Function that appends a str at the end of a txt file,
+    return lenght of str"""
+    open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
