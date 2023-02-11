0d1dd17444e52e3cd60f1364d9374ef0fd7a6d93
rvegarodz <profe.r.vega21@gmail.com>
2023-02-11 15:04:41 -0400

    adding file for task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module provide function called to_json_string()"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """Function return the JSON represantation of an object"""
+    return json.dumps(my_obj)
