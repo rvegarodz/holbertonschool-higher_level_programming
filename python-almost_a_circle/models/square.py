d892272b864a990ebb3e55fad71c37f06fc404c9
rvegarodz <profe.r.vega21@gmail.com>
2023-02-14 12:58:08 -0400

    square file peocho edited

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,6 +4,7 @@
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """Create Square class that inherits from Rectangle class"""
     def __init__(self, size, x=0, y=0, id=None):
