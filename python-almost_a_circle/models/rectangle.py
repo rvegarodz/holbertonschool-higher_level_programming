b838c0f7d4b64b3cc0076b9354a7e37a758afa23
rvegarodz <profe.r.vega21@gmail.com>
2023-02-14 10:05:11 -0400

    adding file for task 7

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -90,7 +90,11 @@ class Rectangle(Base):
 
     def display(self):
         """Display method print the rectangle dimensions with #"""
+        for idy in range(self.__y):
+            print("")
         for i in range(self.__height):
+            for idx in range(self.__x):
+                print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print("")
