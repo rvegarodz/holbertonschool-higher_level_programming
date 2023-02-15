470d4ffdee08b5bb7547b4b96508565af1fcff4a
rvegarodz <profe.r.vega21@gmail.com>
2023-02-15 10:31:25 -0400

    adding files for task 12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,6 +9,37 @@ class Square(Rectangle):
     """Create Square class that inherits from Rectangle class"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+    
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+
+    def update(self, *args, **kwargs):
+        """Method that update arguments of class"""
+        if args:
+            for idx in range(len(args)):
+                if idx == 0:
+                    self.id = args[idx]
+                if idx == 1:
+                    super(__class__, self.__class__).width.__set__(self, args[idx])
+                if idx == 2:
+                    super(__class__, self.__class__).x.__set__(self, args[idx])
+                if idx == 3:
+                    super(__class__, self.__class__).y.__set__(self, args[idx])
+        else:
+            for value in kwargs:
+                if value == "id":
+                    self.id = kwargs[value]
+                if value == "size":
+                    super(__class__, self.__class__).width.__set__(self, kwargs[value])
+                if value == "x":
+                    super(__class__, self.__class__).x.__set__(self, kwargs[value])
+                if value == "y":
+                    super(__class__, self.__class__).y.__set__(self, kwargs[value])
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
