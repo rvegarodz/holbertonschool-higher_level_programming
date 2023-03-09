3de40334c102860650819223e7ee82fc7d168192
rvegarodz <r.vega27@outlook.com>
2023-03-09 13:38:32 -0400

    adding file for task 15

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- Script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server
+SELECT COUNT (score)
+GROUP BY score
+AS values
+ORDER BY score DESC;
