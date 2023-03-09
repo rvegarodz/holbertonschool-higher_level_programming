7cf84a05cf4e8a0a117d0cb2c3fa1f4d7c7996c1
rvegarodz <r.vega27@outlook.com>
2023-03-09 10:56:06 -0400

    adding file for task 10

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- Script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
-SHOW CREATE TABLE second_table;
+SELECT score, name FROM second_table ORDER BY DESC;
