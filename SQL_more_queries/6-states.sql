746d6425f4b4f7daa4f3df6c74cb8248cc88c3fb
rvegarodz <r.vega27@outlook.com>
2023-03-10 16:38:37 -0400

    adding file for task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Script that creates the table id_not_null on your MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT,
+    name VARCHAR(256) NOT NULL,
+    UNIQUE (id),
+    PRIMARY KEY (id)
+);
