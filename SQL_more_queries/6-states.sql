2535d4e36271567644f35fea2af845f0f26314af
rvegarodz <r.vega27@outlook.com>
2023-03-10 16:49:54 -0400

    edited

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Script that creates the table id_not_null on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     UNIQUE (id),
     PRIMARY KEY (id)
