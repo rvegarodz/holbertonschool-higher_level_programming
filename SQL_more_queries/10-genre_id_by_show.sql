1efe21674993b606ff2e0f08fccfc8f75d6b1bfa
rvegarodz <r.vega27@outlook.com>
2023-03-12 18:06:32 -0400

    fix order by again

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,4 @@ SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres 
 RIGHT JOIN tv_shows ON show_id = tv_shows.id 
 WHERE genre_id IS NOT NULL 
-ORDER BY tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
