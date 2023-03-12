4f8b176430afdcef00b1762a6574b8df16d272cb
rvegarodz <r.vega27@outlook.com>
2023-03-12 19:14:35 -0400

    removing NULL of title column

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -3,4 +3,5 @@ SELECT tv_shows.title, tv_genres.name
 FROM tv_show_genres
 RIGHT JOIN tv_shows ON show_id = tv_shows.id
 RIGHT JOIN tv_genres ON genre_id = tv_genres.id
+WHERE title IS NOT NULL
 ORDER BY title, name ASC;
