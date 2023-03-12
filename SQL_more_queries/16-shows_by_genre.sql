6eacc1724a44287333f567ecc0ae9eea6a368222
rvegarodz <r.vega27@outlook.com>
2023-03-12 19:36:55 -0400

    correcting join

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- Script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_genres.name
-FROM tv_show_genres
-RIGHT JOIN tv_shows ON show_id = tv_shows.id
-RIGHT JOIN tv_genres ON genre_id = tv_genres.id
-WHERE tv_shows.title IS NOT NULL
-ORDER BY title, name ASC;
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id =  tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
