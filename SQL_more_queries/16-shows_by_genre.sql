3183203a7d1b516ae0cc06de2af5fbad06a6cff8
rvegarodz <r.vega27@outlook.com>
2023-03-12 19:11:42 -0400

    renaming file and adding order by ...

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_genres.name
+FROM tv_show_genres
+RIGHT JOIN tv_shows ON show_id = tv_shows.id
+RIGHT JOIN tv_genres ON genre_id = tv_genres.id
+ORDER BY title ASC;
