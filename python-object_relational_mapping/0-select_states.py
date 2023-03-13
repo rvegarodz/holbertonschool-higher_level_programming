7a9efad8156c5214311c4a20c8f355ef7340f927
rvegarodz <r.vega27@outlook.com>
2023-03-13 17:44:21 -0400

    add argv

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,45 @@
 #!/usr/bin/python3
 '''Script that lists all states from the database hbtn_0e_0_usa'''
-
 # Module for Connecting To MySQL database
 import MySQLdb
+import sys
 
-# Function for connecting to MySQL database
-database = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="password", db="hbtn_0e_0_usa", charset="utf8")
 
-# Making Cursor Object For Query Execution
-db_cursor = database.cursor()
+def get_states(argv):
+    '''Function that connect and fetch data'''
 
-# Executing Query
-db_cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # Input Arguments
+    i_username = sys.argv[1]
+    i_password = sys.argv[2]
+    i_db = sys.argv[3]
 
-# Fetching Data
-query_rows = db_cursor.fetchall()
+    # Function for connecting to MySQL database
+    database = MySQLdb.connect(host="localhost",
+     port=3306,
+     user=i_username,
+     passwd=i_password,
+     db=i_db,
+     charset="utf8")
 
-# Printing Result of Query
-for row in query_rows:
-    print(row)
+    # Making Cursor Object For Query Execution
+    db_cursor = database.cursor()
 
-# Closing Cursor Object Connection
-db_cursor.close()
+    # Executing Query
+    db_cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-# Closing Database Connection
-database.close()
+    # Fetching Data
+    query_rows = db_cursor.fetchall()
+
+    # Printing Result of Query
+    for row in query_rows:
+        print(row)
+
+    # Closing Cursor Object Connection
+    db_cursor.close()
+
+    # Closing Database Connection
+    database.close()
+
+
+if __name__ == "__main__":
+    get_states(sys.argv[1:])
