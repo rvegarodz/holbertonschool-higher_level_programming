683e5a2160ad207e27234f15e1fb2bdb5475397c
rvegarodz <r.vega27@outlook.com>
2023-03-14 17:03:56 -0400

    file for task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''DOCUMENTATION'''
+# Module for Connecting To MySQL database
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy import MetaData, Table, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'State'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
