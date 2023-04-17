2a7dde8ea9f95865e7cda9b42c6e9134c59eb3b0
rvegarodz <r.vega27@outlook.com>
2023-04-17 13:55:03 -0400

    fix code

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,9 +4,7 @@ const message = 'C is fun';
 if (variables.length <= 2) {
   console.log('Missing number of occurrences');
 } else {
-  if (isNaN(variables[2])) {
-    // do nothing
-  } else {
+  if (!isNaN(variables[2])) {
     for (let i = 0; i < variables[2]; i++) {
       console.log(message);
     }
