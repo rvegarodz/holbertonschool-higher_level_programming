1c79441baf91e2d6d6c8536e964a5566dc955a47
rvegarodz <r.vega27@outlook.com>
2023-04-17 14:11:09 -0400

    adding file for task 8

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const variable = process.argv[2];
+if (variable) {
+  if (!isNaN(variable)) {
+    for (let i = 0; i < variable; i++) {
+      let row = '';
+      for (let j = 0; j < variable; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+} else {
+  console.log('Missing size');
+}
