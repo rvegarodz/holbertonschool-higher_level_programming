aa03317b2df2cb573d319830ed0ee1d7d1e81097
rvegarodz <r.vega27@outlook.com>
2023-04-17 16:05:06 -0400

    fix error

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const variables = process.argv;
 if (variables.length < 4) {
   console.log(0);
 } else {
-  const array = variables.slice(3);
+  const array = variables.slice(2);
   const aInt = array.map(str => parseInt(str));
   aInt.sort((a, b) => b - a);
   console.log(aInt[1]);
