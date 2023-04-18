b776b6914810b9220b6ca895fc46ac8d8d706e08
rvegarodz <r.vega27@outlook.com>
2023-04-18 16:33:53 -0400

    adding file for task 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let char = '';
+    if (typeof c === 'string' || c instanceof String) {
+      char = c;
+    } else {
+      char = 'X';
+    }
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += char;
+        }
+        console.log(row);
+      }
+    }
+  }
+}
+
+module.exports = Square;
