e1470c9fcb324716e7354d899fd6e14a697f5805
rvegarodz <r.vega27@outlook.com>
2023-04-18 22:25:44 -0400

    add converter to base 2

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 exports.converter = function (base) {
+  if (base === 2) {
+    return function (num) {
+      return num.toString(2);
+    };
+  }
   if (base === 10) {
     return function (num) {
       return num.toString(10);
