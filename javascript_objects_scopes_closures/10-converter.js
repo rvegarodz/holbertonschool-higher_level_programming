33243b1272e604c040591e408c5bf8bcdc1ddf2d
rvegarodz <r.vega27@outlook.com>
2023-04-18 22:24:21 -0400

    same without var

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -2,14 +2,12 @@
 exports.converter = function (base) {
   if (base === 10) {
     return function (num) {
-      const result = num.toString(10);
-      return result;
+      return num.toString(10);
     };
   }
   if (base === 16) {
     return function (num) {
-      const result = num.toString(16);
-      return result;
+      return num.toString(16);
     };
   }
 };
