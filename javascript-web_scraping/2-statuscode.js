27fe9292f7ef5f3d50984b70ea74db9883f1f2b1
rvegarodz <r.vega27@outlook.com>
2023-04-21 06:44:35 -0400

    fix semi

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const input = process.argv[2];
 const request = require('request');
-request.get(input).on('response', function(response) {
-    console.log('code: ' + response.statusCode)})
+request.get(input).on('response', function (response) {
+  console.log('code: ' + response.statusCode);
+});
